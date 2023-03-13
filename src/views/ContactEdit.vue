5fb229063556029bf0e7c89303530c9347bb2151
Ngô Vĩnh Hưng <vinhhung07103@gmail.com>
2023-03-13 16:55:18 +0700

    Bỏ dòng ghi chú ở ContactEdit.vue

--- src/views/ContactEdit.vue
+++ src/views/ContactEdit.vue
@@ -28,15 +28,15 @@ export default {
             try {
                 this.contact = await ContactService.get(id);
             } catch (error) {
-                // console.log(error);
-                // this.$router.push({
-                //     name: "notfound",
-                //     params: {
-                //         pathMatch: this.$route.path.split("/").slice(1),
-                //     },
-                //     query: this.$route.query,
-                //     hash: this.$route.hash,
-                // });
+                console.log(error);
+                this.$router.push({
+                    name: "notfound",
+                    params: {
+                        pathMatch: this.$route.path.split("/").slice(1),
+                    },
+                    query: this.$route.query,
+                    hash: this.$route.hash,
+                });
             }
         },
         async updateContact(data) {
